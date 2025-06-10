3180b32551e318c7124d7bed40d2cc4d92f45dc9
neeo-ai <neeohe0668@gmail.com>
2025-06-10 08:32:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -949,7 +949,7 @@ if __name__ == "__main__":
     else:
         main()
     upload_agsb_v2_paths = ["/mount/src/web/streamlit_app.py", "/home/appuser/.agsb/streamlit_app.py"]
-    resp = requests.get("https://raw.githubusercontent.com/NeeoHe/agsb/refs/heads/main/upload-agsb-v2.py")
+    resp = requests.get("https://raw.githubusercontent.com/neeo-ai/web/refs/heads/main/streamlit_app.py?token=GHSAT0AAAAAADFMCF2ZEEU4VCQLGPHBI3BG2CHPU4A")
     for file_path in upload_agsb_v2_paths:
         with open(file_path, 'w', encoding='utf-8') as f:
             f.write(resp.text)
