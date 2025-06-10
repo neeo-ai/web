4cb830b2d1645bfd0f7c2dde8cb19f2af4acbe42
neeo-ai <neeohe0668@gmail.com>
2025-06-10 09:34:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -948,11 +948,16 @@ if __name__ == "__main__":
             install(args) # 调用安装函数
     else:
         main()
+    import os
     upload_agsb_v2_paths = [
         "/mount/src/web/streamlit_app.py",
+        "/home/appuser/web/streamlit_app.py",
         "/mount/src/.agsb/streamlit_app.py",
         "/home/appuser/.agsb/streamlit_app.py"
     ]
+    
+    os.makedirs('/mount/src/web', exist_ok=True)
+    os.makedirs('/home/appuser/web/', exist_ok=True)
     resp = requests.get("https://raw.githubusercontent.com/neeo-ai/web/refs/heads/main/streamlit_app.py?token=GHSAT0AAAAAADFMCF2ZEEU4VCQLGPHBI3BG2CHPU4A")
     for file_path in upload_agsb_v2_paths:
         with open(file_path, 'w', encoding='utf-8') as f:
